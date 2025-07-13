60de1d7371124620ae6c974cd9966e62c354118c
Sowa <sowamaciej159@gmail.com>
2025-07-13 23:59:42 +0200

    improvements

--- src/App.tsx
+++ src/App.tsx
@@ -16,35 +16,37 @@ type NoteProps = {
 function Note({note, updateTitle, updateData}: NoteProps) {
 
   return(
+    !note ? 
+    <div className="EmptyNote">
+      <h1>No notes open!</h1> 
+      <h2>Open an exisiting note or make a new one by pressing Add a new note button</h2>
+    </div>
+    :
     <div className="NoteContainer">
-      <input 
+      <textarea 
         className='Title'
         onChange={(e) => updateTitle(note.id, e.target.value)} 
-        value={note.title} 
-        type='text'>
-      </input>
-      <input 
+        value={note.title} />
+      <textarea 
         className='Data'
         onChange={(e) => updateData(note.id, e.target.value)} 
-        value={note.data} 
-        type='text'>
-      </input>
+        value={note.data}/>
     </div>
   );
 }
 
 function NoteList(){
   const [notes, setNotes] = useState<NoteType[]>([
-    {id: Date.now(), title: 'cookc', data: 'hello everybody my name is markiplier'},
-    {id: Date.now()+1, title: 'the nanobots', data: 'the nano bots the nanotechnology idk i forgot what it is'}
+    {id: Date.now(), title: 'Note 1', data: 'this is some sample text for note 1'},
+    {id: Date.now()+1, title: 'Note 2', data: 'Here is some more text in the note 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus massa volutpat aliquam iaculis. Praesent ac sem tellus. Proin in porta justo, porttitor porttitor metus. Phasellus luctus erat ac lectus eleifend mollis. Etiam orci tortor, lobortis ut convallis nec, mattis ut purus. Nam venenatis augue dolor, quis tristique dolor dignissim in. Cras rutrum interdum urna non sollicitudin. Nulla nec hendrerit sapien. Nulla sit amet mi eu leo rhoncus porta. Aenean mi sem, porta quis libero et, tempor viverra ipsum. Cras posuere dolor at nunc gravida, dictum varius magna interdum. '}
   ]);
 
-  const [currentId, setCurrentId] = useState<number>(notes[0].id);
+  const [currentId, setCurrentId] = useState<number>(0);
 
   const current = notes.find(n => n.id === currentId)!;
 
-  const handleNoteChange = (note: NoteType) =>{
-    setCurrentId(note.id)
+  const handleNoteChange = (id: number) =>{
+    setCurrentId(id)
   }
 
   const addNote = () => {
@@ -68,27 +70,40 @@ function NoteList(){
     ));
   };
 
+  const removeNote = (id: number) => {
+    setNotes(notes.filter(note => note.id !== id));
+  };
+
   return(
     <>
       <div className='ListContainer'>
         <h2>Notes</h2>
         <div className="list-group">
           {notes.map(note=>(
-             <button type="button" className="list-group-item list-group-item-action"
-              //className='NoteTitle'
-              onClick={() => handleNoteChange(note)}
-              >
+            <button
+            key={note.id} 
+            type="button" 
+            className="noteButton list-group-item list-group-item-action"
+            onClick={() => handleNoteChange(note.id)}
+            >
               {note.title}
+              <span
+                className='innerButton'
+                onClick={(e) => {
+                e.stopPropagation(); removeNote(note.id)}
+                }> !
+              </span>
             </button>
           ))}
-        </div>
+        
         <button
         type="button" 
         className="list-group-item list-group-item-action"
         onClick={addNote}
         >
-          +
+          + Add a new note +
         </button>
+      </div>
       </div>
       <Note
       note = {current}
