494606446b3691647e2d384ccd432952b5ba7f6f
Sowa <sowamaciej159@gmail.com>
2025-07-14 13:09:26 +0200

    added a confirm portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { createPortal } from 'react-dom';
 
 type NoteType = {
   id: number,
@@ -8,44 +9,74 @@ type NoteType = {
 };
 
 type NoteProps = {
-  note : NoteType
+  note: NoteType
   updateTitle: (id: number, newTitle: string) => void;
   updateData: (id: number, newData: string) => void;
 }
 
-function Note({note, updateTitle, updateData}: NoteProps) {
+type ModalProps = {
+  onClose: () => void;
+  onConfirm: (id: number | null) => void;
+  item: NoteType | null;
+}
 
-  return(
-    !note ? 
-    <div className="EmptyNote">
-      <h1>No notes open!</h1> 
-      <h2>Open an exisiting note or make a new one by pressing Add a new note button</h2>
-    </div>
-    :
-    <div className="NoteContainer">
-      <textarea 
-        className='Title'
-        onChange={(e) => updateTitle(note.id, e.target.value)} 
-        value={note.title} />
-      <textarea 
-        className='Data'
-        onChange={(e) => updateData(note.id, e.target.value)} 
-        value={note.data}/>
-    </div>
+function ConfirmModal({ onClose, onConfirm, item }: ModalProps) {
+  return createPortal(
+    item != null ?
+    <div className="modal-overlay">
+      <div className="modal-title">
+        Delete note?
+        <button onClick={onClose}>X</button>
+      </div>
+      <div className="modal-content">
+        
+        Are you sure you want to delete {item.title}?
+      </div>
+      <div className="modal-options">
+        <button onClick={onClose}>Cancel</button>
+        <button onClick={() => onConfirm(item.id)}>Delete</button>
+      </div>
+    </div> : null,
+    document.getElementById('modal-root')!
   );
 }
 
-function NoteList(){
+function Note({ note, updateTitle, updateData }: NoteProps) {
+
+  return (
+    !note ?
+      <div className="EmptyNote">
+        <h1>No notes open!</h1>
+        <h2>Open an exisiting note or make a new one by pressing Add a new note button</h2>
+      </div>
+      :
+      <div className="NoteContainer">
+        <textarea
+          className='Title'
+          onChange={(e) => updateTitle(note.id, e.target.value)}
+          value={note.title} />
+        <textarea
+          className='Data'
+          onChange={(e) => updateData(note.id, e.target.value)}
+          value={note.data} />
+      </div>
+  );
+}
+
+function NoteList() {
   const [notes, setNotes] = useState<NoteType[]>([
-    {id: Date.now(), title: 'Note 1', data: 'this is some sample text for note 1'},
-    {id: Date.now()+1, title: 'Note 2', data: 'Here is some more text in the note 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus massa volutpat aliquam iaculis. Praesent ac sem tellus. Proin in porta justo, porttitor porttitor metus. Phasellus luctus erat ac lectus eleifend mollis. Etiam orci tortor, lobortis ut convallis nec, mattis ut purus. Nam venenatis augue dolor, quis tristique dolor dignissim in. Cras rutrum interdum urna non sollicitudin. Nulla nec hendrerit sapien. Nulla sit amet mi eu leo rhoncus porta. Aenean mi sem, porta quis libero et, tempor viverra ipsum. Cras posuere dolor at nunc gravida, dictum varius magna interdum. '}
+    { id: Date.now(), title: 'Note 1', data: 'this is some sample text for note 1' },
+    { id: Date.now() + 1, title: 'Note 2', data: 'Here is some more text in the note 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur faucibus massa volutpat aliquam iaculis. Praesent ac sem tellus. Proin in porta justo, porttitor porttitor metus. Phasellus luctus erat ac lectus eleifend mollis. Etiam orci tortor, lobortis ut convallis nec, mattis ut purus. Nam venenatis augue dolor, quis tristique dolor dignissim in. Cras rutrum interdum urna non sollicitudin. Nulla nec hendrerit sapien. Nulla sit amet mi eu leo rhoncus porta. Aenean mi sem, porta quis libero et, tempor viverra ipsum. Cras posuere dolor at nunc gravida, dictum varius magna interdum. ' }
   ]);
 
   const [currentId, setCurrentId] = useState<number>(0);
 
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [deleteId, setDeteleId] = useState<NoteType | null>(null)
+
   const current = notes.find(n => n.id === currentId)!;
 
-  const handleNoteChange = (id: number) =>{
+  const handleNoteChange = (id: number) => {
     setCurrentId(id)
   }
 
@@ -58,69 +89,85 @@ function NoteList(){
     setNotes([...notes, newNote]);
   };
 
-  const updateTitle = (id: number, newTitle: string) =>{
-    setNotes(notes.map(note=>
+  const updateTitle = (id: number, newTitle: string) => {
+    setNotes(notes.map(note =>
       note.id === id ? { ...note, title: newTitle } : note
     ));
   };
 
-  const updateData = (id: number, newData: string) =>{
-    setNotes(notes.map(note=>
+  const updateData = (id: number, newData: string) => {
+    setNotes(notes.map(note =>
       note.id === id ? { ...note, data: newData } : note
     ));
   };
 
-  const removeNote = (id: number) => {
-    setNotes(notes.filter(note => note.id !== id));
+  const removeNote = (id: number | null) => {
+    id === null ? console.log("remove id null") :
+      setNotes(notes.filter(note => note.id !== id));
+    setShowConfirm(false);
   };
 
-  return(
+  const confirmDelete = (id: NoteType) => {
+    setDeteleId(id);
+    setShowConfirm(true);
+  }
+
+  const handleClose = () => {
+    setDeteleId(null);
+    setShowConfirm(false);
+  }
+
+  return (
     <>
-      <div className='ListContainer'>
-        <h2>Notes</h2>
-        <div className="list-group">
-          {notes.map(note=>(
+      <div className='AppContainer'>
+        <div className='ListContainer'>
+          <h2>Notes</h2>
+          <div className="list-group">
+            {notes.map(note => (
+              <button
+                key={note.id}
+                type="button"
+                className="noteButton list-group-item list-group-item-action"
+                onClick={() => handleNoteChange(note.id)}
+              >
+                {note.title}
+                <span
+                  className='innerButton'
+                  onClick={(e) => {
+                    e.stopPropagation(); confirmDelete(note)
+                  }
+                  }> X
+                </span>
+              </button>
+            ))}
+
             <button
-            key={note.id} 
-            type="button" 
-            className="noteButton list-group-item list-group-item-action"
-            onClick={() => handleNoteChange(note.id)}
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={addNote}
             >
-              {note.title}
-              <span
-                className='innerButton'
-                onClick={(e) => {
-                e.stopPropagation(); removeNote(note.id)}
-                }> !
-              </span>
+              + Add a new note +
             </button>
-          ))}
-        
-        <button
-        type="button" 
-        className="list-group-item list-group-item-action"
-        onClick={addNote}
-        >
-          + Add a new note +
-        </button>
+          </div>
+        </div>
+        <Note
+          note={current}
+          updateTitle={updateTitle}
+          updateData={updateData}
+        />
       </div>
-      </div>
-      <Note
-      note = {current}
-      updateTitle={updateTitle}
-      updateData={updateData}
-      />
+      {showConfirm && (
+        <ConfirmModal onClose={handleClose} onConfirm={removeNote} item={deleteId} />
+      )}
     </>
+
   );
 }
 
 function App() {
 
   return (
-    <div className='AppContainer'>
-      <NoteList/>
-    </div>
-
+    <NoteList />
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <div id="modal-root"></div>
   </StrictMode>,
 )
